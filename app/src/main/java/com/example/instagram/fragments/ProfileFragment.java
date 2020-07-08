9f5df331c56f54f0fb685920711408aad9a618a8
jacqueline-he <jacquelinehe00@gmail.com>
2020-07-08 14:01:47 -0700

    User can change profile pic

--- app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -1,16 +1,23 @@
 package com.example.instagram.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,8 +35,12 @@ import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment {
@@ -43,6 +54,11 @@ public class ProfileFragment extends Fragment {
     private ParseUser currentUser;
     private TextView tvUsername;
     private ImageView ivProfileImage;
+    private EndlessRecyclerViewScrollListener scrollListener;
+    boolean infScroll = false;
+    private String photoFileName = "photo.jpg";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    private File photoFile;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -102,12 +118,94 @@ public class ProfileFragment extends Fragment {
         rvPosts.setLayoutManager(gridLayoutManager);
         queryUserPosts();
 
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               launchCamera();
+
+            }
+        });
+
+        scrollListener = new EndlessRecyclerViewScrollListener(gridLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                infScroll = true;
+                Log.i(TAG, "Asking for inf scroll posts");
+                queryUserPosts();
+            }
+        };
+        rvPosts.addOnScrollListener(scrollListener);
+
 
     }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.instagram", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Bitmap taken = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // ivProfileImage.setImageBitmap(taken);
+                Glide.with(this).load(photoFile.getAbsolutePath()).transform(new CircleCrop()).into(ivProfileImage);
+                ParseFile file = new ParseFile(photoFile);
+                currentUser.put("profilepic", file);
+                currentUser.saveInBackground();
+            } else { // Result was a failure
+                Toast.makeText(getActivity(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 
     private void queryUserPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
+
+        if (infScroll && userPosts.size() > 0) {
+            Date oldest = userPosts.get(userPosts.size() - 1).getCreatedAt();
+            Log.i(TAG, "Getting inf scroll posts");
+            query.whereLessThan("createdAt", oldest);
+            infScroll = false;
+        }
+
         query.setLimit(24);
         query.addDescendingOrder("createdAt");
         Log.e(TAG, "Id: " + currentUser.getObjectId());
